c37f30e91cec6ab8e26eb4fb890b929f65573ede
jangidlaxman <90422419+jangidlaxman@users.noreply.github.com>
2021-11-30 17:04:58 -0600

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAiknahHTD87fqNV6B_jIcIgNZVLosFyPk",
+    authDomain: "kwitter-7da3f.firebaseapp.com",
+    projectId: "kwitter-7da3f",
+    storageBucket: "kwitter-7da3f.appspot.com",
+    messagingSenderId: "580565786268",
+    appId: "1:580565786268:web:b50e664c67c77a60309759"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
